fa0605cbc0acc74119ae85669299c06d091cb813
piseak02 <piseak02@gmail.com>
2025-02-15 01:58:58 +0700

    แก้ธีมทุกอย่าง

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,10 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "หน้าแรก",
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 226, 74, 28)),
-          useMaterial3: true),
+        primaryColor: Colors.orange,
+      ),
+      title: "หน้าแรก",
       home: MyHomepaga(),
     );
   }
@@ -24,58 +23,120 @@ class Myapp extends StatelessWidget {
 class MyHomepaga extends StatefulWidget {
   const MyHomepaga({super.key});
 
-  @overridee
+  @override
   State<MyHomepaga> createState() => _MyHomepagaState();
 }
 
 class _MyHomepagaState extends State<MyHomepaga> {
+  int number = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 70,
-        leading: Container(
-          padding: EdgeInsets.only(leftSafeArea: 10),
-          alignment: Alignment.centerLeft,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(
-                Icons.person,
-                size: 28,
-                color: const Color.fromARGB(255, 37, 39, 39),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Text("แสดงชื่อผู้ใช้",
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(70),
+          child: AppBar(
+            titleSpacing: 10,
+            toolbarHeight: 70,
+            centerTitle: false, //  ทำให้ title อยู่ชิดซ้าย
+            title: Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  size: 28,
+                  color: const Color.fromARGB(255, 37, 39, 39),
+                ),
+                SizedBox(width: 20),
+                Text(
+                  "แสดงชื่อผู้ใช้",
                   style: TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.bold,
-                      color: Colors.redAccent)),
+                      color: Colors.redAccent),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+
+            actions: [
+              IconButton(
+                icon: Icon(Icons.notifications),
+                onPressed: () {},
+              ),
             ],
           ),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {},
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/ipad.jpg"), // ✅ ใช้รูปพื้นหลัง
+              fit: BoxFit.cover, // ✅ ปรับให้รูปเต็มจอ
+            ),
           ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("สวัสดีครับ"),
-            Text("hello dart"),
-            Text("hello flutter"),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "เพิ่มจำนวน",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                Text(
+                  number.toString(),
+                  style: TextStyle(
+                    fontSize: 60,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor:
+              Color.fromARGB(240, 51, 51, 47), // ✅ เปลี่ยนสีพื้นหลังของแถบบาร์
+          selectedItemColor: const Color.fromARGB(255, 235, 157, 40),
+          unselectedItemColor: const Color.fromARGB(255, 228, 221, 221),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                ),
+                label: "หน้าแรก"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.credit_card,
+                ),
+                label: "บัญชี"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.menu,
+                ),
+                label: "เมนูหลัก"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.receipt,
+                ),
+                label: "บิลลูกค้า"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.business,
+                ),
+                label: "บิลซัพพายเออร์"),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              number++;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
